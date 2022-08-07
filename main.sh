8155c22149812999587a60bd614a3cb3dc893cfe
gityzon <109445070+gityzon@users.noreply.github.com>
2022-08-07 18:23:41 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,5 @@
 #！警告：replit免费版中所有代码都是公开可见的，建议使用edu版部署
-#如何更新OneNav-Extend：
-#1.停止运行，备份数据（下载data目录中的几个db3格式的文件，如果自己有改php的文件也记得自行备份）
-#2.删除所有文件，只保留nginx和main.sh
-#3.再次RUN，不要安装，将db3文件上传到data目录中，然后直接访问你的站点，everything going well~
+#
 
 #!/bin/bash
 
@@ -19,5 +16,5 @@ unzip typecho.zip
 rm -f typecho.zip
 fi
 
-php -S 0.0.0.0:8000 -t . &
+php -S 0.0.0.0:8000 -t .
 nginx -g 'daemon off;'

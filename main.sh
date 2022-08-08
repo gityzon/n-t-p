b5f652095771dbb27473cb3906f7d120a8dfd8af
gityzon <109445070+gityzon@users.noreply.github.com>
2022-08-08 22:48:52 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #！警告：replit免费版中所有代码都是公开可见的，建议使用edu版部署
 #！注意：安装Typecho时请选择PDO驱动的sqlite驱动器（如无远程SQL）
 #！警告：为弥补ftp文件管理功能，在网站目录/tfm 下预制了
-#！ Tiny File Manager，初始帐号为admin；密码：admin@123，请尽快修改
+#！ Tiny File Manager，初始管理帐号为admin；密码：admin@123，请尽快修改（参考：https://github.com/prasathmani/tinyfilemanager/wiki/Security-and-User-Management）
 
 
 #!/bin/bash

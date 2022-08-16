03641e506da3f6d60e5c37ffe6bc19dbba0a06e0
gityzon <109445070+gityzon@users.noreply.github.com>
2022-08-16 16:25:03 +0800

    Create start-redis.sh

---
+++ start-redis.sh
@@ -0,0 +1,18 @@
+# shut down redis-server (if applicable)
+
+REDIS_SERVER_PID_FILE=/tmp/redis-server.pid
+(kill $(cat $REDIS_SERVER_PID_FILE) 2>&1) >/dev/null
+sleep 0.1
+
+
+# start redis-server
+
+nohup redis-server redis.conf >/dev/null 2>&1 &
+sleep 0.1
+echo $! > $REDIS_SERVER_PID_FILE
+
+
+# start redis-cli
+
+#redis-cli < commands.txt
+#redis-cli
